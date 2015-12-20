94e9df45b51ddb4b971bd3de6ec7529661366775
陆晨 <bolizhazi@live.cn>
2015-12-20 22:28:38 +0800

    备份

---
+++ kefu/Controllers/CustomerServiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kefu.Controllers
+{
+    public class CustomerServiceController : Controller
+    {
+        // GET: CustomerService
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
